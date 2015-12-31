fef7bfa602d12febf42150c30b10dc7b0c22c3cc
Yacine Hmito <yacine.hmito@gmail.com>
2015-12-31 14:18:23 +0100

    The yarn framework.
    - `default.nix` is a function that takes the library set as an argument
      and returns the yarn set
    - In the yarn set, the attribute devices is the set of the complete
      device modules
    - A complete device module imports the corresponding module found via
      the `all-devices.nix` set, every module in the `module-list.nix` set
      and overrides `pkgs` with the set given by `all-packages.nix`
    
    Updated README

---
+++ devices/all-devices.nix
@@ -0,0 +1,8 @@
+# This set should hold an attribute for each device.
+# The value of the attribute is the path to the corresponding module.
+# Profiles should not belong to this set.
+lib:
+
+{
+
+}
