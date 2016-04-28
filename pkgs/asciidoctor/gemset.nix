ca7c7137d10ac6bf62a57524c36e3c754cb3ebfe
gpyh <yacine.hmito@gmail.com>
2016-04-28 20:02:22 +0200

    Added bespoke to asciidoctor

--- pkgs/asciidoctor/gemset.nix
+++ pkgs/asciidoctor/gemset.nix
@@ -1,21 +1,59 @@
 {
-  "asciidoctor" = {
-    version = "1.5.4";
+  asciidoctor = {
     source = {
-      type = "gem";
       remotes = ["https://rubygems.org"];
       sha256 = "0xrli1fjsf1f0h6d9g55vzivxbqac2ygawcacx5ijnqn522wg9qc";
-    };
-  };
-  "asciidoctor-diagram" = {
-    version = "1.4.0";
-    source = {
       type = "gem";
+    };
+    version = "1.5.4";
+  };
+  asciidoctor-bespoke = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1awy933sswxvi2hxpll3rh9phxcvmqhrbb91m6ibjchnf7qsl3zk";
+      type = "gem";
+    };
+    version = "1.0.0.alpha.1";
+  };
+  asciidoctor-diagram = {
+    dependencies = ["asciidoctor"];
+    source = {
       remotes = ["https://rubygems.org"];
       sha256 = "0yb2gqzzbvgf5im0bhv26s3h09m9m6a0pjlq3swqcvwi1szc64k5";
+      type = "gem";
     };
-    dependencies = [
-      "asciidoctor"
-    ];
+    version = "1.4.0";
+  };
+  slim = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1szs71hh0msm5gj6qbcxw44m3hqnwybx4yh02scwixnwg576058k";
+      type = "gem";
+    };
+    version = "3.0.6";
+  };
+  temple = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0ysraljv7lkb04z5vdyrkijab7j1jzj1mgz4bj82744dp7d0rhb0";
+      type = "gem";
+    };
+    version = "0.7.6";
+  };
+  thread_safe = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1hq46wqsyylx5afkp6jmcihdpv4ynzzq9ygb6z2pb1cbz5js0gcr";
+      type = "gem";
+    };
+    version = "0.3.5";
+  };
+  tilt = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0lkd40xfdqkp333vdfhrfjmi2y7k2hjs4azawfb62mrkfp7ivj84";
+      type = "gem";
+    };
+    version = "2.0.2";
   };
 }
