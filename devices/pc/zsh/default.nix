b3e73e35d958ac952100538c3789e4a6bc2faedf
gpyh <yacine.hmito@gmail.com>
2017-01-11 00:07:32 +0100

    Getting rid of zgen in favor of NixOS stuff
    Making use of:
    - oh-my-zsh
    - zsh-syntax-highlighting
    - zsh-autosuggestions
    All packaged for Nix
    (zgen is faster but zgen is impure)
    Also added bashcompinit

--- devices/pc/zsh/default.nix
+++ devices/pc/zsh/default.nix
@@ -1,17 +1,31 @@
 # My user configuration.
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   readFile = builtins.readFile;
 in
 {
+  environment.systemPackages = [
+    pkgs.oh-my-zsh
+  ];
+
   programs.zsh = {
     shellInit = readFile ./init.zsh;
-    interactiveShellInit = readFile ./interactive.zsh;
+    interactiveShellInit = lib.concatStrings [
+      ''
+        export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
+        ZSH_THEME="fishy"
+        plugins=(git)
+        source $ZSH/oh-my-zsh.sh
+      ''
+      (readFile ./interactive.zsh)
+    ];
     loginShellInit = readFile ./login.zsh;
-    promptInit = readFile ./prompt.zsh;
     shellAliases = import ./aliases.nix;
+    promptInit = ""; # Avoid conflict with oh-my-zsh
     enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    enableAutosuggestions = true;
     enable = true;
   };
   users.defaultUserShell = "/run/current-system/sw/bin/zsh";
--- devices/pc/zsh/interactive.zsh
+++ devices/pc/zsh/interactive.zsh
@@ -1,25 +1,3 @@
-# Zgen plugin manager
-source "/home/gpyh/.local/share/zgen.git/zgen.zsh"
-
-if ! zgen saved; then
-  echo "Creating a zgen save"
-  
-  zgen oh-my-zsh
-  
-  # Vi keybindings
-  zgen oh-my-zsh plugins/vi-mode
-  
-  # Fish-like experience
-  zgen load zsh-users/zsh-syntax-highlighting
-  zgen load zsh-users/zsh-autosuggestions
-  zgen oh-my-zsh themes/fishy
-
-  # Colored man page
-  zgen oh-my-zsh plugins/colored-man-pages
-  
-  zgen save
-fi
-
 # Fix problems between tiling WM and IntelliJ Idea
 export _JAVA_AWT_WM_NONREPARENTING=1
 
@@ -48,6 +26,10 @@ setopt promptsubst
 autoload -U promptinit
 promptinit
 
+# Bash completion compatibility mode
+autoload bashcompinit
+bashcompinit
+
 # Menu interface to select completion
 zstyle ':completion:*' menu select=1
 
@@ -56,7 +38,6 @@ mkdir -p $HOME/.zsh/cache
 export CACHEDIR="$HOME/.zsh/cache"
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path $CACHEDIR
-compinit -d $CACHEDIR/zcompdump
 
 # Accept autosuggestion
 bindkey '^f' autosuggest-accept
