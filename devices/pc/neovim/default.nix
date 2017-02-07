4572c55f4aa5c82316a550f2bded64523cac4e24
gpyh <yacine.hmito@gmail.com>
2017-02-07 13:29:44 +0100

    Editorconfig for vim

--- devices/pc/neovim/default.nix
+++ devices/pc/neovim/default.nix
@@ -1,9 +1,27 @@
-{ lib, fetchFromGitHub, vimUtils, vimPlugins, neovim }:
+{ lib, fetchFromGitHub, vimUtils, vimPlugins, neovim, editorconfig-core-c }:
 
 let
   customPlugins = import ./plugins.nix {
     inherit fetchFromGitHub vimUtils;
   };
+  
+  localVimrc = ''
+    if $XDG_CONFIG_HOME == ""
+      let config = expand("$HOME/.config/nvim/init.vim")
+    else
+      let config = expand("$XDG_CONFIG_HOME/nvim/init.vim")
+    endif
+    if filereadable(config)
+      exec "source " . config
+    else
+      echo "Could not read " . config
+    endif
+  '';
+
+  editorConfigVimrc = ''
+    let g:EditorConfig_exec_path = "${editorconfig-core-c}/bin/editorconfig"
+    let g:EditorConfig_core_mode = "external_command"
+  '';
 in
 neovim.override {
   withPython = true;
@@ -26,6 +44,7 @@ neovim.override {
         # Integration
         { name = "fugitive"; }
         { name = "neomake"; }
+        { name = "editorconfig-vim"; }
 
         # Ease of use
         { name = "commentary"; } # Use gcc to comment a line
@@ -41,22 +60,13 @@ neovim.override {
         { name = "idris-vim"; }
         { name = "vim-javascript"; }
         { name = "vim-jsx"; }
+        { name = "typescript-vim"; }
       ];
     };
     customRC = builtins.readFile ./init.vim +
                builtins.readFile ./lightline.vim +
                builtins.readFile ./digraphs.vim +
-    ''
-      if $XDG_CONFIG_HOME == ""
-        let config = expand("$HOME/.config/nvim/init.vim")
-      else
-        let config = expand("$XDG_CONFIG_HOME/nvim/init.vim")
-      endif
-      if filereadable(config)
-        exec "source " . config
-      else
-        echo "Could not read " . config
-      endif
-    '';
+               editorConfigVimrc +
+               localVimrc;
   };
 }
